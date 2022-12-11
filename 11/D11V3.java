b5f85dd9f8d07d4b687437368996b48f2e8d3a81
Hilbert Schraal <w.h.schraal@port-ability.com>
2022-12-11 11:30:06 +0100

    11

--- 11/D11V3.java
+++ 11/D11V3.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +9,7 @@ import java.util.regex.Pattern;
 public class D11V3 {
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         if (args.length != 1) {
             System.out.println("Need to specify the input file");
             System.exit(0);
@@ -21,7 +21,8 @@ public class D11V3 {
             } else {
                 try {
                     List<String> allLines = Files.readAllLines(Paths.get(inputFile));
-                    long result = doIt(allLines);
+                    System.out.println("time: "+(System.currentTimeMillis()-start));
+                    long result = doIt(allLines, start);
                     System.out.println(result);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -30,12 +31,18 @@ public class D11V3 {
         }
     }
 
-    private static long doIt(List<String> allLines) {
+    private static long doIt(List<String> allLines, long start) {
 
         //read the monkeys
         List<Monkey> monkeys = new ArrayList<>();
         int monkeyIndex = 0;
         long sumTestValues = 1;
+        Pattern testPattern = Pattern.compile("Test: divisible by ([0-9]+)");
+        Pattern truePattern = Pattern.compile("If true: throw to monkey ([0-9]+)");
+        Pattern falsePattern = Pattern.compile("If false: throw to monkey ([0-9]+)");
+        Pattern opsPattern = Pattern.compile("(.+) ([+|*]) (.+)");
+
+
         for (String l : allLines) {
             String line = l.stripLeading();
             if (line.startsWith("Monkey")) {
@@ -43,31 +50,20 @@ public class D11V3 {
             } else if (line.startsWith("Starting items: ")) {
                 String items = line.split(": ")[1];
                 for (String i : items.split(", ")) {
-                    monkeys.get(monkeyIndex).things.add(Long.parseLong(i));
+                    monkeys.get(monkeyIndex).items.add(Long.parseLong(i));
                 }
             } else if (line.startsWith("Operation:")) {
                 String operation = line.split(" = ")[1];
-                String patternString = "(.+) ([+|*]) (.+)";
-                Pattern pattern = Pattern.compile(patternString);
-                Matcher matcher = pattern.matcher(operation);
-                if (matcher.find()) {
-                    monkeys.get(monkeyIndex).operationX = matcher.group(1);
-                    monkeys.get(monkeyIndex).operator = matcher.group(2);
-
-                    monkeys.get(monkeyIndex).operationY = matcher.group(3).equals("old") ? 0 : Long.parseLong(matcher.group(3));
-                } else {
-                    System.out.println("could not match "+patternString);
-                }
+                List<String> ops = extractValues(opsPattern, operation);
+                monkeys.get(monkeyIndex).operator = ops.get(1);
+                monkeys.get(monkeyIndex).operationY = ops.get(2).equals("old") ? 0 : Long.parseLong(ops.get(2));
             } else if (line.startsWith("Test:")) {
-                String patternString = "Test: divisible by ([0-9]+)";
-                monkeys.get(monkeyIndex).testValue = Long.parseLong(extractValue(patternString, line));
+                monkeys.get(monkeyIndex).testValue = Long.parseLong(extractValues(testPattern, line).get(0));
                 sumTestValues = sumTestValues *monkeys.get(monkeyIndex).testValue;
             } else if (line.startsWith("If true")) {
-                String patternString = "If true: throw to monkey ([0-9]+)";
-                monkeys.get(monkeyIndex).trueMonkey = Integer.parseInt(extractValue(patternString, line));
+                monkeys.get(monkeyIndex).trueMonkey = Integer.parseInt(extractValues(truePattern, line).get(0));
             } else if (line.startsWith("If false")) {
-                String patternString = "If false: throw to monkey ([0-9]+)";
-                monkeys.get(monkeyIndex).falseMonkey = Integer.parseInt(extractValue(patternString, line));
+                monkeys.get(monkeyIndex).falseMonkey = Integer.parseInt(extractValues(falsePattern, line).get(0));
             } else if (line.isEmpty()) {
                 //next monkey
                 System.out.println(monkeys.get(monkeyIndex));
@@ -76,34 +72,26 @@ public class D11V3 {
                 System.out.println("unknown line");
             }
         }
+        System.out.println("time: "+(System.currentTimeMillis()-start));
 
         //play rounds
-        long start = System.currentTimeMillis();
         for (int round = 0; round < 10000; round++) {
             for (Monkey monkey : monkeys) {
-                for (long thing : monkey.things) {
-                    long worryLevel = computeNew(thing, monkey.operator, monkey.operationY);
-                    worryLevel = worryLevel%sumTestValues;
-                    if (worryLevel%monkey.testValue == 0) {
-                        monkeys.get(monkey.trueMonkey).things.add(worryLevel);
+                for (long thing : monkey.items) {
+                    long y = monkey.operationY ==0 ? thing : monkey.operationY;
+                    long worryLevel = monkey.operator.equals("+") ? thing + y : thing * y;
+                    worryLevel = worryLevel % sumTestValues;
+                    if (worryLevel % monkey.testValue == 0) {
+                        monkeys.get(monkey.trueMonkey).items.add(worryLevel);
                     } else {
-                        monkeys.get(monkey.falseMonkey).things.add(worryLevel);
+                        monkeys.get(monkey.falseMonkey).items.add(worryLevel);
                     }
                     monkey.numberOfActions++;
-                    monkey.things.removeFirst();
                 }
+                monkey.items.clear();
             }
-//            if (round%100 == 0) {
-//                System.out.println("round: "+round);
-//                System.out.println("time: "+(System.currentTimeMillis()-start));
-//                long[] values = new long[monkeys.size()];
-//                for (int i = 0 ; i < monkeys.size(); i++) {
-//                    values[i] = monkeys.get(i).numberOfActions;
-//                }
-//                Arrays.sort(values);
-//                System.out.println(Arrays.toString(values));
-//            }
         }
+        System.out.println("time: "+(System.currentTimeMillis()-start));
 
         System.out.println();
         long[] values = new long[monkeys.size()];
@@ -115,27 +103,25 @@ public class D11V3 {
         return values[values.length-2] * values[values.length-1];
     }
 
-    private static long computeNew(long i, String operator, long operationY) {
-        long y = operationY==0 ? i : operationY;
-
-        return operator.equals("+")? i+y : i*y;
+    private static List<String> extractValues(String patternString, String line) {
+        Pattern pattern = Pattern.compile(patternString);
+        return extractValues(pattern, line);
     }
 
-    private static String extractValue(String patternString, String line) {
-        Pattern pattern = Pattern.compile(patternString);
+    private static List<String> extractValues(Pattern pattern, String line) {
         Matcher matcher = pattern.matcher(line);
-        String result = null;
+        List<String> result = new ArrayList<>();
         if (matcher.find()) {
-            result = matcher.group(1);
+            for (int i = 1; i <= matcher.groupCount(); i++)
+                result.add(matcher.group(i));
         } else {
-            System.out.println("could not match "+patternString);
+            System.out.println("could not match "+pattern);
         }
         return result;
     }
 
     private static class Monkey {
-        Deque<Long> things = new ArrayDeque<>();
-        String operationX;
+        List<Long> items = new ArrayList<>();
         long operationY;
         String operator;
         long testValue;
@@ -146,8 +132,7 @@ public class D11V3 {
         @Override
         public String toString() {
             return "Monkey{" +
-                    "things=" + things +
-                    ", operationX='" + operationX + '\'' +
+                    "items=" + items +
                     ", operationY='" + operationY + '\'' +
                     ", operator='" + operator + '\'' +
                     ", testValue=" + testValue +
